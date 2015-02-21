8f77b5d6d02c30d2c0ae7f6db700f83e9c0e6760
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-02-21 14:03:12 +0100

    Added stickybar to homepage

--- js/stickybar.js
+++ js/stickybar.js
@@ -1,15 +1,41 @@
-var triggerHeight = $('#customerBar').height();
-triggerHeight += $('#searchBar').height();
-triggerHeight += $('#menuBar').height();
+var triggerHeight = $('.customerBar').height();
+triggerHeight += $('.searchBar').height();
 
-$('#searchBar').affix({
+$('#sticky').affix({
     offset: {
         top: triggerHeight
     }
 });
-
-$('#customerBar').on('affix-top.bs.affix', function(e) {
-    this.css("display", "none");
+$('#sticky').on('affix.bs.affix', function() {
+    $('#sticky').hide();
+    $('.menuBar').css({
+        'display' : 'none',
+        'position' : 'fixed',
+        'top' : '70px',
+        'width' : '100%',
+        '-webkit-transform' : 'translate3d(0,0,0)',
+        '-o-transform' : 'translate3d(0,0,0)',
+        'transform' : 'translate3d(0,0,0)',
+        'z-index' : '5000'
+    });
 });
 
-console.log($('#customerBar'));
+$('#sticky').on('affixed.bs.affix', function() {
+    $('#sticky').slideDown('fast');
+});
+
+$('#sticky').on('affix-top.bs.affix', function() {
+    $('.menuBar').css({
+        'display' : 'block',
+        'position' : 'relative',
+        'top' : '0'
+    });
+});
+
+$('#toggleCategories').click(function() {
+    if($('.menuBar').is(":visible")) {
+        $('.menuBar').slideUp();
+    } else {
+        $('.menuBar').slideDown();
+    }
+});
