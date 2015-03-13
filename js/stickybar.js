e0a12e70842c5de00aac79e1b9442901bdd04170
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-03-13 14:49:03 +0100

    Edit stickyheader

--- js/stickybar.js
+++ js/stickybar.js
@@ -16,10 +16,20 @@ $('#sticky').on('affix.bs.affix', function() {
         '-webkit-transform' : 'translate3d(0,0,0)',
         '-o-transform' : 'translate3d(0,0,0)',
         'transform' : 'translate3d(0,0,0)',
-        'z-index' : '5000',
+        'z-index' : '4999',
         '-webkit-box-shadow' : '0px 5px 10px 0px rgba(50, 50, 50, 0.5)',
         '-moz-box-shadow' : '0px 5px 10px 0px rgba(50, 50, 50, 0.5)',
         'box-shadow' : '0px 5px 10px 0px rgba(50, 50, 50, 0.5)'
+    });
+    $('.customerBar').css({
+        'display' : 'none',
+        'position' : 'fixed',
+        'top' : '0',
+        'width' : '100%',
+        '-webkit-transform' : 'translate3d(0,0,0)',
+        '-o-transform' : 'translate3d(0,0,0)',
+        'transform' : 'translate3d(0,0,0)',
+        'z-index' : '5000'
     });
 });
 
@@ -37,12 +47,34 @@ $('#sticky').on('affix-top.bs.affix', function() {
         '-moz-box-shadow' : 'none',
         'box-shadow' : 'none'
     });
+    $('.customerBar').css({
+        'display' : 'block',
+        'position' : 'relative',
+        'top' : '0',
+        'z-index' : '0'
+    });
 });
 
 $('#toggleCategories').click(function() {
     if($('.menuBar').is(":visible")) {
-        $('.menuBar').slideUp();
+        var slideMenuBarUp = $('.menuBar').slideUp();
+        $.when(slideMenuBarUp).done(function() {
+            $('.menuBar').animate({
+                'top' : '70px'
+            });
+            $('.customerBar').slideUp();
+            $('#sticky').animate({
+                'top' : '0'
+            });
+        });
     } else {
+        $('.customerBar').slideDown();
+        $('#sticky').animate({
+            'top' : '30px'
+        });
+        $('.menuBar').animate({
+            'top' : '100px'
+        });
         $('.menuBar').slideDown();
     }
 });
